45233ab51a2b32b6af362c682f9ebe63bf82d5e0
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-11-16 11:03:05 -0500

    add new_record and find_records methods to FMCloudInstance class and add documentation

--- distant_vfx.py
+++ distant_vfx.py
@@ -176,6 +176,20 @@ class ShotgunInstance:
 class FMCloudInstance:
 
     def __init__(self, host_url, username, password, database, user_pool_id, client_id, api_version='vLatest'):
+        """
+        Create an new FMCloudInstance object. Uses the specified params to connect to a FileMaker Cloud hosted DB.
+        :param host_url: The base url of the FileMaker Cloud server.
+        :param username: The Claris ID username used to sign in to FileMaker Cloud.
+        :param password: The Claris ID password used to sign in to FileMaker Cloud.
+        :param database: The name of the database to connect to.
+        :param user_pool_id: The Userpool_ID value used to retrieve the FMID token required to authenticate with
+                             FileMaker Cloud via Amazon Cognito. This can be obtained per instructions here:
+                             https://help.claris.com/en/customer-console-help/content/create-fmid-token.html
+        :param client_id: The Client_ID value used to retrieve the FMID token required to authenticate with
+                             FileMaker Cloud via Amazon Cognito. This can be obtained per instructions here:
+                             https://help.claris.com/en/customer-console-help/content/create-fmid-token.html
+        :param api_version: The FileMaker Cloud Data API version to use for API calls. This is set to latest by default.
+        """
         self.host_url = host_url
         self.username = username
         self.password = password
@@ -199,46 +213,109 @@ class FMCloudInstance:
             LOG.error(f'{exc_type}: {exc_val}')
 
     def login(self):
-        # Establish connection to FMP Cloud DB via the sessions api endpoint.
+        """
+        Establish connection to the specified FMP Cloud DB via the sessions api endpoint.
+        :return: None.
+        """
+
+        # Get the FMID token from Amazon Cognito.
         self._get_fmid_token()
-        print(self._fmid_token)
+
+        # Set the url and headers and perform the login api request.
         headers = {
             'Content-Type': 'application/json',
             'Authorization': f'FMID {self._fmid_token}'
         }
         url = self.host_url + f'/fmi/data/{self.api_version}/databases/{self.database}/sessions'
         response = requests.post(url, headers=headers, data='{}')
-        if response.ok:
-            print('Login response: ' + response.text)
-            try:
-                response_json = response.json()
-                self._bearer_token = response_json.get('response').get('token')
-                print(self._bearer_token)
-            except json.decoder.JSONDecodeError as e:
-                LOG.error(e)
-                print(e)
+
+        # Using raise_for_status here since if login fails, we can't do anything else.
+        response.raise_for_status()
+
+        # Extract bearer token from json response (used for all subsequent API calls).
+        response_json = response.json()
+        self._bearer_token = response_json.get('response').get('token')
 
     def logout(self):
-        # Log out of the current DB session.
+        """
+        Log out of the current DB session. Called automatically when leaving context if using context manager.
+        :return: None.
+        """
+
+        # Set headers and url and perform the logout api request.
         headers = {'Content-Type': 'application/json'}
         url = self.host_url + f'/fmi/data/{self.api_version}/databases/{self.database}/sessions/{self._bearer_token}'
         response = requests.delete(url, headers=headers)
-        print('Logout response: ' + response.text)
+
+        # If the response is valid, reset the bearer token.
         if response.ok:
-            print(response.text)
             self._bearer_token = None
-            # TODO: log this
 
     def new_record(self, layout, data):
+        """
+        Create a new record on the given layout with the provided field names.
+        :param layout: The layout on which to create a new record.
+        :param data: A dict of field names and values, e.g. { "String Field": "value_1", "Number Field": 99.99,
+                     "repetitionField(1)" : "fieldValue"}
+        :return: The unique record ID of the newly created record (use to access the record in subsequent calls).
+        """
+
+        # Set headers and url for the request.
         headers = {
             'Content-Type': 'application/json',
             'Authorization': f'Bearer {self._bearer_token}'
         }
         url = self.host_url + f'/fmi/data/{self.api_version}/databases/{self.database}/layouts/{layout}/records'
-        record_data = json.dumps({'fieldData': data})
-        response = requests.post(url, headers=headers, data=record_data)
+
+        # Convert new record data to JSON.
+        request_json = json.dumps({'fieldData': data})
+
+        # Perform the api request.
+        response = requests.post(url, headers=headers, data=request_json)
+
+        # If successful, convert the response to json and return the newly created record ID.
         if response.ok:
-            print(response)
+            response_json = response.json()
+            record_id = response_json.get('response').get('recordId')
+            return record_id
+
+    def find_records(self, layout, query, sort=None, limit=None):
+        """
+        Perform a find request matching one or more queries on the given layout.
+        :param layout: The name of the layout on which to perform the request.
+        :param query: A list of dicts specifying the find parameters, e.g. [{"Group": "=Surgeon"}, {"Field2: "=val2"}]
+                      Use "omit": "true" to perform omit requests, e.g. [{"Work State" : "NY", "omit" : "true"}]
+        :param sort: A list of dicts specifying the sort parameters, e.g. [{"fieldName": "Work State","sortOrder":
+                     "ascend"}, {"fieldName": "First Name", "sortOrder": "ascend"}]
+        :param limit: Limit the number of records returned, e.g. a value of 1 will return 1 record.
+        :return: List of record dicts matching the find request params.
+        """
+
+        # Set the headers and url.
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self._bearer_token}'
+        }
+        url = self.host_url + f'/fmi/data/{self.api_version}/databases/{self.database}/layouts/{layout}/_find'
+
+        # At minimum, query must be provided.
+        data = {'query': query}
+
+        # Add sort or limit params if provided.
+        if sort is not None:
+            data['sort'] = sort
+        if limit is not None:
+            data['limit'] = limit
+
+        # Convert request data to json and perform the request.
+        request_json = json.dumps(data)
+        response = requests.post(url, headers=headers, data=request_json)
+
+        # If successful, extract and return the list of record data.
+        if response.ok:
+            response_json = response.json()
+            response_data = response_json.get('response').get('data')
+            return response_data
 
     def _get_fmid_token(self):
         # Get the FMID token for FMP Cloud login via Amazon Cognito.
