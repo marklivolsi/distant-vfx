a76b3a21e77cd721787705c505684b3e341ef64b
Alex Hughes <ahughesalex@gmail.com>
2020-11-23 12:03:11 -0800

    Implement subclasses of NukeQuickTimeFarmJob
    We have 2 end user classes we have created and one middle class.
    
    DistantSlateFarmJob implements the concept of a slate image and 3 slate
    text inputs. Currently, top left, top right, bottom right.
    
    DistantH264FarmJob implements a custom template.nk rendering out an h264
    movie.
    
    DistantDNxHDFarmJob implements a custom template.nk rendering out a
    DNxHD115 movie.

---
+++ python/distant_vfx/farm/jobs/distant_h264/job.py
@@ -0,0 +1,26 @@
+import os
+
+from distant_vfx.farm.jobs import distant_slate
+
+
+class DistantH264FarmJob(distant_slate.DistantSlateFarmJob):
+    NUKESCRIPT_PATH = os.path.join(os.path.dirname(__file__), "template.nk")
+
+    @classmethod
+    def help(cls):
+        message = super(DistantH264FarmJob, cls).help()
+
+        return (
+            message
+            + """
+  DistantH264FarmJob
+==============================
+A DistantH264FarmJob subclasses a DistantSlateFarmJob with specific settings for H264
+
+:param str|None slate_image: Path to the slate image we want to use.
+:param str|None slate_left_text: Text for the top left side of the slate.
+:param str|None slate_right_text: Text for the bottom right side of the slate.
+:param str|None slate_bottom_text: Text for the bottom right of the slate.
+
+"""
+        )
