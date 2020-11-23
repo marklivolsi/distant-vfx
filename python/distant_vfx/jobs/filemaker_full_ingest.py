875f5ba7276adb1f5769e998dc9445df96cae16b
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-11-23 14:11:41 -0500

    add filemaker_full_ingest job

---
+++ python/distant_vfx/jobs/filemaker_full_ingest.py
@@ -0,0 +1,56 @@
+from ..config import Config
+from ..filemaker import FMCloudInstance
+
+
+def main(versions_dict_list, filename_dict_list, package_dict):
+    """
+    Inject new vendor submission data into FileMaker Cloud.
+
+    :param versions_dict_list: A list of version dicts containing version data. Format each dict like:
+                               version_dict = { 'Filename': <filename>,
+                                                'DeliveryPackage': <package>,
+                                                'Status': <subStatus>,
+                                                'DeliveryNote': <subNote> }
+
+    :param filename_dict_list: A list of filename dicts containing filename data. Format each dict like:
+                               filename_dict = { 'Filename': <filename> }
+    :param package_dict: A single dictionary describing the package data. Format like:
+                         package_dict = { 'package': <package_name>,
+                                          'path': <package_path> }
+    :return: None.
+    """
+
+    # TODO: Replace with os.environ variables
+    config = Config()
+    config_did_load = config.load_config()
+    data = config.data
+    url = data['fmp_data_api']['base_url']
+    username = data['fmp_data_api']['username']
+    password = data['fmp_data_api']['password']
+    user_pool_id = data['fmp_data_api']['cognito_userpool_id']
+    client_id = data['fmp_data_api']['cognito_client_id']
+
+    # Inject new versions from vendor csv doc
+    if versions_dict_list:
+        database = data['fmp_data_api']['databases']['vfx']
+        with FMCloudInstance(url, username, password, database, user_pool_id, client_id) as fmp:
+            versions_layout = 'api_Versions_form'
+            for version_dict in versions_dict_list:
+                version_record = fmp.new_record(versions_layout, version_dict)
+
+    # Inject filename and package records
+    if filename_dict_list and package_dict:
+        database = data['fmp_data_api']['databases']['admin']
+        with FMCloudInstance(url, username, password, database, user_pool_id, client_id) as fmp:
+            transfers_layout = 'api_Transfers_form'
+            pkg_record_id = fmp.new_record(transfers_layout, package_dict)
+            pkg_data = fmp.get_record(transfers_layout, record_id=pkg_record_id)
+            pkg_primary_key = pkg_data.get('fieldData').get('PrimaryKey')
+
+            transfers_data_layout = 'api_TransfersData_form'
+            for filename_dict in filename_dict_list:
+                if pkg_primary_key is not None:
+                    filename_dict['Foriegnkey'] = pkg_primary_key
+                filename_record = fmp.new_record(transfers_data_layout, filename_dict)
+
+                # TODO: Upload .log file to container field.
--- python/distant_vfx/jobs/launch_ldq_review.py
+++ python/distant_vfx/jobs/launch_ldq_review.py
@@ -5,12 +5,6 @@ def main():
 
     # TODO: Access FMP creds with environ variable
 
-    host_url = None
-    username = None
-    password = None
-    database = None
-    user_pool_id = None
-
     from ..config import Config
 
     config = Config()
@@ -22,22 +16,21 @@ def main():
     database = data['fmp_data_api']['databases']['vfx']
     user_pool_id = data['fmp_data_api']['cognito_userpool_id']
     client_id = data['fmp_data_api']['cognito_client_id']
-    layout = 'api_Notes_form'
 
     with FMCloudInstance(url, username, password, database, user_pool_id, client_id) as fmp:
 
         review_query = [
-            {'Reviews::ReviewType': 'Supervisor'},
-            {'Reviews::ReviewStatus': 'Open'}
+            {'SupeReviewFlag': 1}
         ]
         notes_layout = 'api_Notes_form'
 
         records = fmp.find_records(notes_layout, review_query)
 
         review_files = []
-        for record in records:
-            field_data = record.get('fieldData')
-            filename = field_data.get('Filename')
-            if filename:
-                review_files.append(filename)
+        if records:
+            for record in records:
+                field_data = record.get('fieldData')
+                filename = field_data.get('Filename')
+                if filename:
+                    review_files.append(filename)
         print(review_files)
