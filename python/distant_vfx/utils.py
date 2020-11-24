9025bd87d6c9ff13026a88f7f0d1a6135307b8de
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-11-24 10:30:10 -0500

    add email handler class

--- python/distant_vfx/utils.py
+++ python/distant_vfx/utils.py
@@ -1,7 +1,32 @@
+import yagmail
 
 
 class EmailHandler:
 
-    def __init__(self):
-        pass
+    def __init__(self, gmail_username, gmail_password):
+        self.client = yagmail.SMTP(gmail_username, gmail_password)
 
+    def send(self, recipients, subject, contents):
+        self.client.send(to=recipients, subject=subject, contents=contents)
+
+    @staticmethod
+    def format_html_table(rows):
+
+        # Format the header row
+        header_row = rows.pop(0)
+        header_html = ''
+        for col in header_row:
+            header_html += f'<th>{col}</th>'
+
+        html = f'<table><thead><tr>{header_html}</tr></thead><tbody>'
+
+        # Format table rows
+        row_html = ''
+        for row in rows:
+            row_html += '<tr>'
+            for col in row:
+                row_html += f'<td>{col}</td>'
+            row_html += '</tr>'
+
+        html += row_html + '</tbody></table>'
+        return html
