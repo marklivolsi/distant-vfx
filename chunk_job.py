e9708871d55a8ac3f310d2d71aadcdee2f3707a2
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-11-25 13:41:45 -0500

    add help to argparse arguments

--- chunk_job.py
+++ chunk_job.py
@@ -6,9 +6,19 @@ import argparse
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', action='store', type=str, required=True)
-    parser.add_argument('-m', '--move', action='store_true')
-    parser.add_argument('-i', '--input', action='store', nargs='+', required=True)
+    parser.add_argument('-o', '--output',
+                        action='store',
+                        type=str,
+                        required=True,
+                        help='<required> Specify an output directory.')
+    parser.add_argument('-m', '--move',
+                        action='store_true',
+                        help='Set chunk operation to move (rather than copy).')
+    parser.add_argument('-i', '--input',
+                        action='store',
+                        nargs='+',
+                        required=True,
+                        help='<required> Specify one or more directory paths whose contents should be chunked.')
 
     args = parser.parse_args()
     chunk_to_new_packages.main(args.output,
