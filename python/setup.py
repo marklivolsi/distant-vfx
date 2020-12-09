72d03bf2d0b9cd5ba66b80726e0e2c7c92d4fc98
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-12-09 13:38:19 -0500

    version update

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open('../README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='distant_vfx',
-    version='0.2.2',
+    version='0.2.3',
     author='Mark Livolsi',
     author_email='mark.c.livolsi@gmail.com',
     description='A toolkit for the Distant VFX team.',
