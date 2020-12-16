972d76758ded1eac96443cec36ff324db00380aa
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-12-16 11:46:35 -0500

    add entry point for reel inject

---
+++ job_entry_points/reel_inject.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import argparse
+from distant_vfx.jobs import inject_reel_edl
+
+
+# An entry point for the inject_reel_edl job
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-i', '--input',
+        action='store',
+        nargs='+',
+        type=str,
+        required=True,
+        help='<required> Specify one or more scan order edl file paths.'
+    )
+
+    parser.add_argument(
+        '--csv',
+        action='store_true',
+        help='Specifying --csv will output scan order data to csv files instead of injecting to FileMaker.'
+    )
+
+    args = parser.parse_args()
+
+    for edl_path in args.input:
+        inject_reel_edl.main(edl_path, csv_out=args.csv)
+
+
+if __name__ == '__main__':
+    main()
