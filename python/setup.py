0a1f450af50f364c46442d80ba8edec26dcf9d48
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-12-15 23:29:37 -0500

    update long description

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,12 @@
 import setuptools
 
-# with open('../README.md', 'r', encoding='utf-8') as fh:
-#     long_description = fh.read()
-long_description = ''
-
 setuptools.setup(
     name='distant_vfx',
     version='0.2.5',
     author='Mark Livolsi',
     author_email='mark.c.livolsi@gmail.com',
     description='A toolkit for the Distant VFX team.',
-    long_description=long_description,
+    long_description='A toolkit for the Distant VFX production team.',
     long_description_content_type='text/markdown',
     url='https://github.com/marklivolsi/distant-vfx',
     packages=setuptools.find_packages(),
