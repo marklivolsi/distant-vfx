67b61a537564e1ed76a843abb5abc0046a11a541
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-12-18 14:15:32 -0500

    add job to generate missing movs from frame ranges (wip)

--- python/distant_vfx/jobs/generate_missing_movs.py
+++ python/distant_vfx/jobs/generate_missing_movs.py
@@ -1,8 +1,51 @@
 from collections import defaultdict
 import os
 
+from ..farm.jobs.distant_dnx115 import DistantDNxHDFarmJob
+from ..farm.jobs.distant_h264 import DistantH264FarmJob
+
 
 IMG_SEQ_EXTS = ['exr']
+
+
+def _render_dnx115(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
+                   notes, width=1920, height=1080):
+    dnxhd_job = DistantDNxHDFarmJob(
+        image_path=image_path,
+        output_path=output_path,
+        nukescript_path=nukescript_template_path,
+        width=width,
+        height=height,
+        text_tl=os.path.basename(output_path).split('.')[0],
+        text_br='[value frame]',
+        first_frame=first_frame,
+        last_frame=last_frame,
+        name=scene_name + ' - DNxHD 115 Render',
+        slate_left_text=os.path.basename(output_path).split('.')[0],
+        slate_right_text=username,
+        slate_bottom_text=notes
+    )
+    job_id = dnxhd_job.submit()
+
+
+def _render_h264(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
+                 notes, width=1920, height=1080):
+    h264_job = DistantH264FarmJob(
+        image_path=image_path,
+        output_path=output_path,
+        nukescript_path=nukescript_template_path,
+        width=width,
+        height=height,
+        text_tl=os.path.basename(output_path).split('.')[0],
+        text_br='[value frame]',
+        first_frame=first_frame,
+        last_frame=last_frame,
+        name=scene_name + ' - DNxHD 115 Render',
+        slate_left_text=os.path.basename(output_path).split('.')[0],
+        slate_right_text=username,
+        slate_bottom_text=notes
+    )
+    job_id = h264_job.submit()
 
 
 def _parse_img_seq_paths(basename_map):
