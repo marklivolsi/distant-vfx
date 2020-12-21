0e532f40b0b5df9ecbb7d0f515cb7361b31d25e2
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-12-21 11:03:00 -0500

    add render code

--- job_entry_points/generate_missing_movs.py
+++ job_entry_points/generate_missing_movs.py
@@ -4,83 +4,119 @@
 
 from collections import defaultdict
 import os
+import sys
 
-# from python.distant_quicktime.farm.jobs.distant_dnx115 import DistantDNxHDFarmJob
-# from python.distant_quicktime.farm.jobs.distant_h264 import DistantH264FarmJob
-
-
-IMG_SEQ_EXTS = ['exr']
-MOVIE_EXTS = ['mov', 'mp4']
+from python.distant_quicktime.farm.jobs.distant_dnx115 import DistantDNxHDFarmJob
+from python.distant_quicktime.farm.jobs.distant_h264 import DistantH264FarmJob
 
 
 def main(root_path):
     files = _scan_files(root_path)
-    basename_map = _make_basename_map(files)
+    basename_map, frame_map = _make_basename_map(files)
     for basename, ext_map in basename_map.items():
 
         exr_path = ext_map.get('exr')
         dnx_path = ext_map.get('dnx')
         h264_path = ext_map.get('h264')
 
-        print(exr_path, dnx_path, h264_path)
-
         if exr_path is None:
-            continue
+            continue  # skip it if we don't have exr
         else:
             exr_container_path = os.path.dirname(exr_path)
 
+        parent_path = os.path.dirname(exr_container_path)
+        width, height = _get_qt_dimensions(parent_path)
+        resolution_folder_base = str(width) + 'x' + str(height)
+        filename = basename + '.mov'
+
+        first_frame, last_frame = min(frame_map[basename]), max(frame_map[basename])
+
         if dnx_path is None:
-            pass  # render dnx
+            resolution_folder_name = resolution_folder_base + '_dnx115'
+            output_path = os.path.join(parent_path, resolution_folder_name, filename)
+            _render_dnx115(
+                image_path=exr_path,
+                output_path=output_path,
+                nukescript_template_path=None,
+                first_frame=first_frame,
+                last_frame=last_frame,
+                scene_name=basename,
+                username='Distant VFX',
+                notes='Plate check render',
+                width=width,
+                height=height
+            )
 
         if h264_path is None:
-            pass  # render h264
+            resolution_folder_name = resolution_folder_base + '_h264'
+            output_path = os.path.join(parent_path, resolution_folder_name, filename)
+            _render_h264(
+                image_path=exr_path,
+                output_path=output_path,
+                nukescript_template_path=None,
+                first_frame=first_frame,
+                last_frame=last_frame,
+                scene_name=basename,
+                username='Distant VFX',
+                notes='Plate check render',
+                width=width,
+                height=height
+            )
 
 
+def _get_qt_dimensions(parent_path):
+    basename = os.path.basename(parent_path)
+    split = basename.split('_')
+    split2 = split[0].split('x')
+    width, height = split2[0], split2[1]
+    return int(width), int(height)
 
-# def _render_dnx115(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
-#                    notes, width=1920, height=1080):
-#     dnxhd_job = DistantDNxHDFarmJob(
-#         image_path=image_path,
-#         output_path=output_path,
-#         nukescript_path=nukescript_template_path,
-#         width=width,
-#         height=height,
-#         text_tl=os.path.basename(output_path).split('.')[0],
-#         text_br='[value frame]',
-#         first_frame=first_frame,
-#         last_frame=last_frame,
-#         name=scene_name + ' - DNxHD 115 Render',
-#         slate_left_text=os.path.basename(output_path).split('.')[0],
-#         slate_right_text=username,
-#         slate_bottom_text=notes,
-#         priority=1
-#     )
-#     job_id = dnxhd_job.submit()
-#
-#
-# def _render_h264(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
-#                  notes, width=1920, height=1080):
-#     h264_job = DistantH264FarmJob(
-#         image_path=image_path,
-#         output_path=output_path,
-#         nukescript_path=nukescript_template_path,
-#         width=width,
-#         height=height,
-#         text_tl=os.path.basename(output_path).split('.')[0],
-#         text_br='[value frame]',
-#         first_frame=first_frame,
-#         last_frame=last_frame,
-#         name=scene_name + ' - DNxHD 115 Render',
-#         slate_left_text=os.path.basename(output_path).split('.')[0],
-#         slate_right_text=username,
-#         slate_bottom_text=notes,
-#         priority=1
-#     )
-#     job_id = h264_job.submit()
+
+def _render_dnx115(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
+                   notes, width=1920, height=1080):
+    dnxhd_job = DistantDNxHDFarmJob(
+        image_path=image_path,
+        output_path=output_path,
+        nukescript_path=nukescript_template_path,
+        width=width,
+        height=height,
+        text_tl=os.path.basename(output_path).split('.')[0],
+        text_br='[value frame]',
+        first_frame=first_frame,
+        last_frame=last_frame,
+        name=scene_name + ' - DNxHD 115 Render',
+        slate_left_text=os.path.basename(output_path).split('.')[0],
+        slate_right_text=username,
+        slate_bottom_text=notes,
+        priority=1
+    )
+    job_id = dnxhd_job.submit()
+
+
+def _render_h264(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
+                 notes, width=1920, height=1080):
+    h264_job = DistantH264FarmJob(
+        image_path=image_path,
+        output_path=output_path,
+        nukescript_path=nukescript_template_path,
+        width=width,
+        height=height,
+        text_tl=os.path.basename(output_path).split('.')[0],
+        text_br='[value frame]',
+        first_frame=first_frame,
+        last_frame=last_frame,
+        name=scene_name + ' - DNxHD 115 Render',
+        slate_left_text=os.path.basename(output_path).split('.')[0],
+        slate_right_text=username,
+        slate_bottom_text=notes,
+        priority=1
+    )
+    job_id = h264_job.submit()
 
 
 def _make_basename_map(filepath_list):
     basename_map = defaultdict(dict)
+    frame_map = defaultdict(list)
 
     for path in filepath_list:
         filename = os.path.basename(path)
@@ -97,10 +133,22 @@ def _make_basename_map(filepath_list):
             elif 'h264' in path_lower:
                 key = 'h264'
 
-        if key is not None:
-            basename_map[basename][key] = path
+        if key is None:
+            continue
 
-    return basename_map
+        if key in 'exr':
+            frame = split[-2]
+
+            frame_map[basename].append(int(frame))
+
+            split[-2] = '%{}d'.format(str(len(frame)).zfill(2))
+            exr_filename_fmt = '.'.join(split)
+            dir_name = os.path.dirname(path)
+            path = os.path.join(dir_name, exr_filename_fmt)
+
+        basename_map[basename][key] = path
+
+    return basename_map, frame_map
 
 
 def _scan_files(root_path):
@@ -111,4 +159,4 @@ def _scan_files(root_path):
 
 
 if __name__ == '__main__':
-    main('/Users/marklivolsi/Desktop/LUM_20200117_01')
+    main(sys.argv[1])
