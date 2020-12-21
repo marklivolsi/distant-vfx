e26c42ad8475d22d767911b8da9f03250674a05b
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-12-21 10:14:29 -0500

    update job logic

--- job_entry_points/generate_missing_movs.py
+++ job_entry_points/generate_missing_movs.py
@@ -5,84 +5,101 @@
 from collections import defaultdict
 import os
 
-from python.distant_quicktime.farm.jobs.distant_dnx115 import DistantDNxHDFarmJob
-from python.distant_quicktime.farm.jobs.distant_h264 import DistantH264FarmJob
+# from python.distant_quicktime.farm.jobs.distant_dnx115 import DistantDNxHDFarmJob
+# from python.distant_quicktime.farm.jobs.distant_h264 import DistantH264FarmJob
 
 
 IMG_SEQ_EXTS = ['exr']
+MOVIE_EXTS = ['mov', 'mp4']
 
 
-def main():
-    pass
+def main(root_path):
+    files = _scan_files(root_path)
+    basename_map = _make_basename_map(files)
+    for basename, ext_map in basename_map.items():
+
+        exr_path = ext_map.get('exr')
+        dnx_path = ext_map.get('dnx')
+        h264_path = ext_map.get('h264')
+
+        print(exr_path, dnx_path, h264_path)
+
+        if exr_path is None:
+            continue
+        else:
+            exr_container_path = os.path.dirname(exr_path)
+
+        if dnx_path is None:
+            pass  # render dnx
+
+        if h264_path is None:
+            pass  # render h264
 
 
-def _render_dnx115(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
-                   notes, width=1920, height=1080):
-    dnxhd_job = DistantDNxHDFarmJob(
-        image_path=image_path,
-        output_path=output_path,
-        nukescript_path=nukescript_template_path,
-        width=width,
-        height=height,
-        text_tl=os.path.basename(output_path).split('.')[0],
-        text_br='[value frame]',
-        first_frame=first_frame,
-        last_frame=last_frame,
-        name=scene_name + ' - DNxHD 115 Render',
-        slate_left_text=os.path.basename(output_path).split('.')[0],
-        slate_right_text=username,
-        slate_bottom_text=notes,
-        priority=1
-    )
-    job_id = dnxhd_job.submit()
+
+# def _render_dnx115(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
+#                    notes, width=1920, height=1080):
+#     dnxhd_job = DistantDNxHDFarmJob(
+#         image_path=image_path,
+#         output_path=output_path,
+#         nukescript_path=nukescript_template_path,
+#         width=width,
+#         height=height,
+#         text_tl=os.path.basename(output_path).split('.')[0],
+#         text_br='[value frame]',
+#         first_frame=first_frame,
+#         last_frame=last_frame,
+#         name=scene_name + ' - DNxHD 115 Render',
+#         slate_left_text=os.path.basename(output_path).split('.')[0],
+#         slate_right_text=username,
+#         slate_bottom_text=notes,
+#         priority=1
+#     )
+#     job_id = dnxhd_job.submit()
+#
+#
+# def _render_h264(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
+#                  notes, width=1920, height=1080):
+#     h264_job = DistantH264FarmJob(
+#         image_path=image_path,
+#         output_path=output_path,
+#         nukescript_path=nukescript_template_path,
+#         width=width,
+#         height=height,
+#         text_tl=os.path.basename(output_path).split('.')[0],
+#         text_br='[value frame]',
+#         first_frame=first_frame,
+#         last_frame=last_frame,
+#         name=scene_name + ' - DNxHD 115 Render',
+#         slate_left_text=os.path.basename(output_path).split('.')[0],
+#         slate_right_text=username,
+#         slate_bottom_text=notes,
+#         priority=1
+#     )
+#     job_id = h264_job.submit()
 
 
-def _render_h264(image_path, output_path, nukescript_template_path, first_frame, last_frame, scene_name, username,
-                 notes, width=1920, height=1080):
-    h264_job = DistantH264FarmJob(
-        image_path=image_path,
-        output_path=output_path,
-        nukescript_path=nukescript_template_path,
-        width=width,
-        height=height,
-        text_tl=os.path.basename(output_path).split('.')[0],
-        text_br='[value frame]',
-        first_frame=first_frame,
-        last_frame=last_frame,
-        name=scene_name + ' - DNxHD 115 Render',
-        slate_left_text=os.path.basename(output_path).split('.')[0],
-        slate_right_text=username,
-        slate_bottom_text=notes,
-        priority=1
-    )
-    job_id = h264_job.submit()
+def _make_basename_map(filepath_list):
+    basename_map = defaultdict(dict)
 
-
-def _parse_img_seq_paths(basename_map):
-    img_seq_paths = []
-    for basename, file_list in basename_map.items():
-        for (ext, path) in file_list:
-            if ext in IMG_SEQ_EXTS:
-                parent_dir = os.path.dirname(path)
-                img_seq_paths.append(parent_dir)
-    return img_seq_paths
-
-
-def _make_basename_map(filepath_list, include_ext):
-    basename_map = defaultdict(list)
     for path in filepath_list:
         filename = os.path.basename(path)
         split = filename.split('.')
         basename, ext = split[0], split[-1]
-        if ext in include_ext:
-            if basename_map[basename]:
-                for item in basename_map[basename]:
-                    if ext in item[0]:
-                        break  # only grab one frame of a framerange for each basename
-                    else:
-                        basename_map[basename].append((ext, path))
-            else:
-                basename_map[basename].append((ext, path))
+        path_lower = path.lower()
+
+        key = None
+        if (ext in 'exr') and ('exr' not in basename_map):
+            key = 'exr'
+        elif ext in 'mov':
+            if 'dnx' in path_lower:
+                key = 'dnx'
+            elif 'h264' in path_lower:
+                key = 'h264'
+
+        if key is not None:
+            basename_map[basename][key] = path
+
     return basename_map
 
 
@@ -94,4 +111,4 @@ def _scan_files(root_path):
 
 
 if __name__ == '__main__':
-    main()
+    main('/Users/marklivolsi/Desktop/LUM_20200117_01')
