b06f10f7b67de2b07b5cb7ded6736a75510d568b
Mark Livolsi <mark.c.livolsi@gmail.com>
2020-12-24 15:07:42 -0500

    update scan order inject to inject stills

--- python/distant_vfx/jobs/inject_scan_order_edl.py
+++ python/distant_vfx/jobs/inject_scan_order_edl.py
@@ -1,9 +1,10 @@
 import csv
 import os
-import time
-from fmrest import CloudServer
-from fmrest.exceptions import BadJSON
-from ..constants import FMP_URL, FMP_USERNAME, FMP_PASSWORD, FMP_EDIT_DB, FMP_SCANS_LAYOUT
+import traceback
+
+from ..filemaker import CloudServerWrapper
+from ..constants import FMP_URL, FMP_USERNAME, FMP_PASSWORD, FMP_EDIT_DB, FMP_ADMIN_DB, \
+    FMP_SCANS_LAYOUT, FMP_IMAGES_LAYOUT, FMP_PROCESS_IMAGE_SCRIPT
 from . import edl_lowercase_loc
 
 
@@ -143,30 +144,63 @@ def _dict2csv(events, csv_filename):  # export csv from dictionary NEED TO RENAM
             csv_out.writerow(event)
 
 
-def _inject_scan_edl(edl_dict, tries=3):
-    with CloudServer(url=FMP_URL,
-                     user=FMP_USERNAME,
-                     password=FMP_PASSWORD,
-                     database=FMP_EDIT_DB,
-                     layout=FMP_SCANS_LAYOUT
-                     ) as fmp:
+def _inject_scan_edl(edl_dict):
+    with CloudServerWrapper(url=FMP_URL,
+                            user=FMP_USERNAME,
+                            password=FMP_PASSWORD,
+                            database=FMP_EDIT_DB,
+                            layout=FMP_SCANS_LAYOUT
+                            ) as fmp:
         fmp.login()
+
+        # create scan records
         for line in edl_dict:
-            for i in range(tries):
+            try:
+                record_id = fmp.create_record(line)
+            except:
+                traceback.print_exc()
+
+
+def _find_stills(root_path):
+    for root, dirs, files in os.walk(root_path):
+        for filename in files:
+            ext = os.path.splitext(filename)[1]
+            if ext in ['.jpg', '.jpeg', '.png']:
+                filepath = os.path.join(root, filename)
+                yield filename, filepath
+
+
+def _inject_stills(root_path):
+    stills = _find_stills(root_path)
+    if not stills:
+        return
+
+    with CloudServerWrapper(url=FMP_URL,
+                            user=FMP_USERNAME,
+                            password=FMP_PASSWORD,
+                            database=FMP_ADMIN_DB,
+                            layout=FMP_IMAGES_LAYOUT
+                            ) as fmp:
+        fmp.login()
+        for still in stills:
+            image_did_upload = False
+            try:
+                filename, filepath = still[0], still[1]
+                record_data = {'Filename': filename}
+                record_id = fmp.create_record(record_data)
+                file = open(filepath, 'rb')
+                image_did_upload = fmp.upload_container(record_id, field_name='Image', file_=file)
+                file.close()
+            except:
+                traceback.print_exc()
+
+            if image_did_upload:
                 try:
-                    record_id = fmp.create_record(line)
-                except BadJSON as e:
-                    if i <= tries - 1:
-                        time.sleep(0.5)
-                        continue
-                    else:
-                        print(f'Error: {e} \n Response: {e._response}')
-                        break
-                except Exception as e:
-                    print(e)
-                    break
-                else:
-                    break
+                    image_record = fmp.get_record(record_id)
+                    primary_key = image_record.PrimaryKey
+                    script_result = fmp.perform_script(FMP_PROCESS_IMAGE_SCRIPT, param=primary_key)
+                except:
+                    traceback.print_exc()
 
 
 def main(edl_path, csv_out=False):
@@ -174,6 +208,8 @@ def main(edl_path, csv_out=False):
     edl_dict = _edl2dict(edl_path)
     if not csv_out:
         _inject_scan_edl(edl_dict)
+        root_path = os.path.dirname(edl_path)
+        _inject_stills(root_path)
     else:
         csv_path = edl_path.rsplit('.', 1)[0] + '.csv'
         _dict2csv(edl_dict, csv_path)
