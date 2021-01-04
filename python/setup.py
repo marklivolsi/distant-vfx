70204313ddd8228371221c36b869cd8bc571c65a
Mark Livolsi <mark.c.livolsi@gmail.com>
2021-01-04 16:59:54 -0500

    version up

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='distant_vfx',
-    version='0.2.9',
+    version='0.3.0',
     author='Mark Livolsi',
     author_email='mark.c.livolsi@gmail.com',
     description='A toolkit for the Distant VFX team.',
