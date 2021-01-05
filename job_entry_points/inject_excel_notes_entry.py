ef46998896df99ba04fe7ce75812f4582af2bff6
Mark Livolsi <mark.c.livolsi@gmail.com>
2021-01-05 11:31:04 -0500

    add entry point for inject_excel_notes job

---
+++ job_entry_points/inject_excel_notes_entry.py
@@ -0,0 +1,21 @@
+import argparse
+from distant_vfx.jobs import inject_excel_notes
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('xlsx_paths',
+                        type=str,
+                        nargs='+',
+                        help='Specify one or more file paths for .xlsx note documents to import to FileMaker.')
+
+    args = parser.parse_args()
+
+    for path in args.xlsx_paths:
+        inject_excel_notes.main(xlsx_path=path)
+
+
+if __name__ == '__main__':
+    main()
