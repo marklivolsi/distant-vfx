374473e088cde808724b34348bc662c244eba3af
Mark Livolsi <mark.c.livolsi@gmail.com>
2021-01-07 15:55:46 -0500

    update additional faspex methods

--- python/distant_vfx/aspera.py
+++ python/distant_vfx/aspera.py
@@ -11,16 +11,16 @@ class AscpTransfer:
 
 class FaspexSession:
 
-    def __init__(self, url, user, password, admin=False):
+    def __init__(self, url, user, password):
         self.url = url
         self.user = user
         self.password = password
-        self.admin = admin
         self.last_processed_package_id = None
         self.latest_package_id = None
         self._token = None
         self._refresh_token = None
         self._headers = {}
+        self._user_id = None
         self._set_content_type()
 
     def login(self):
@@ -36,6 +36,16 @@ class FaspexSession:
         )
         self._token = response.get('access_token')
         self._refresh_token = response.get('refresh_token')
+        # user_data = self.fetch_user_data()
+        # self._user_id = int(user_data.get('id'))
+
+    def fetch_user_data(self):
+        api_path = self._format_api_path(FASPEX_API_PATHS['users'])
+        user_data = self._call_faspex(
+            method='GET',
+            api_path=api_path
+        )
+        return user_data
 
     def fetch_transfer_specs(self, package_id, transfer_direction):
         if transfer_direction not in ['send', 'receive']:
@@ -43,9 +53,9 @@ class FaspexSession:
         api_path = self._format_api_path(FASPEX_API_PATHS['transfer_specs'],
                                          package_id=package_id)
         transfer_specs = self._call_faspex(
-            method='GET',
+            method='POST',
             api_path=api_path,
-            data={'direction': transfer_direction}
+            data={'direction': transfer_direction},
         )
         return transfer_specs
 
@@ -57,15 +67,25 @@ class FaspexSession:
         )
         return packages
 
+    def fetch_one_package(self, package_id):
+        api_path = FASPEX_API_PATHS['packages'] + '/{package_id}'
+        api_path = self._format_api_path(api_path,
+                                         package_id=package_id)
+        package_data = self._call_faspex(
+            method='GET',
+            api_path=api_path,
+        )
+        return package_data
+
     def get_latest_package_id(self, packages_list):
-        package_ids = [self._get_package_id(package) for package in packages_list]
+        package_ids = [self.get_package_id(package) for package in packages_list]
         self.latest_package_id = max(package_ids)
         return self.latest_package_id
 
     def get_packages_to_process(self, packages_list):
         if self.last_processed_package_id:
             packages_to_process = [package for package in packages_list if
-                                   self._get_package_id(package) > self.last_processed_package_id]
+                                   self.get_package_id(package) > self.last_processed_package_id]
             return packages_to_process
         else:
             return None
@@ -74,7 +94,7 @@ class FaspexSession:
         try:
             with open(json_file, 'r') as file:
                 data = json.load(file)
-                self.last_processed_package_id = self._get_package_id(data)
+                self.last_processed_package_id = self.get_package_id(data)
         except FileNotFoundError:
             self.last_processed_package_id = None
         finally:
@@ -92,13 +112,14 @@ class FaspexSession:
                 json.dump(json_dict, file)
 
     @staticmethod
-    def _get_package_id(package_json):
+    def get_package_id(package_json):
         return package_json.get('id')
 
     def _call_faspex(self, method, api_path, data=None, params=None, **kwargs):
         url = self.url + api_path
         request_data = json.dumps(data) if data else None
         self._update_token_header()
+        print(url, self._token, self._headers, request_data)
         response = requests.request(
             method=method,
             headers=self._headers,
@@ -110,8 +131,8 @@ class FaspexSession:
         return response.json()
 
     def _format_api_path(self, api_path, **kwargs):
-        if self.admin:
-            user_id = self.user
+        if self._user_id:
+            user_id = self._user_id
         else:
             user_id = 'me'
         return api_path.format(user_id=user_id, **kwargs)
