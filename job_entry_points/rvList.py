4bc75301f1c301aad1925c703009c3cbaa8261d9
Mark Livolsi <mark.c.livolsi@gmail.com>
2021-01-18 16:35:20 -0500

    update rvList code to correctly offset frame ranges

--- job_entry_points/rvList.py
+++ job_entry_points/rvList.py
@@ -3,7 +3,9 @@
 import argparse
 import os
 import subprocess
-from python.distant_vfx.constants import SHOT_TREE_BASE_PATH, RV_PATH
+from python.distant_vfx.filemaker import CloudServerWrapper
+from python.distant_vfx.constants import SHOT_TREE_BASE_PATH, RV_PATH, FMP_URL, FMP_USERNAME, FMP_PASSWORD, FMP_VFX_DB, \
+    FMP_VERSIONS_LAYOUT
 
 
 def main():
@@ -33,22 +35,30 @@ def main():
     latest_elem = _find_latest_dnx(elem_files)
 
     files = []
+
+    try:
+        version_records = _find_version_data(latest_comp)
+        record = version_records[0]
+        frame_offset, range_start = _get_frame_offset_and_range_start_from_version_data(record)
+    except:
+        frame_offset, range_start = 17, 17
+
     if latest_comp:
-        comp_arg = _format_comp_arg(latest_comp, 17)  # todo: get the offset from the db
+        comp_arg = _format_comp_arg(file=latest_comp, range_start=range_start)
         files += comp_arg
     else:
         print(f'No comps found for shot {shot}.')
-    if latest_elem:
-        # files.append(latest_elem)
-        elem_arg = _format_comp_arg(latest_elem, 17)
-        files += elem_arg
 
+    if latest_elem:
+        elem_arg = _format_comp_arg(file=latest_elem, range_start=range_start)
+        files += elem_arg
     else:
         print(f'No elements found for shot {shot}.')
+
     if plate_files:
         plate_args = []
         for plate in plate_files:
-            plate_arg = _format_plate_arg(plate, 17)
+            plate_arg = _format_plate_arg(plate, frame_offset=frame_offset)
             plate_args += plate_arg
         files += plate_args
     else:
@@ -60,6 +70,8 @@ def main():
 def _launch_rv(files):
     cmd = [RV_PATH, '-wipe'] + files
 
+    print(cmd)
+
     process = subprocess.Popen(cmd,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE,
@@ -68,12 +80,51 @@ def _launch_rv(files):
     stdout, stderr = process.communicate()
 
 
-def _format_comp_arg(file, start_frame_shift):
-    return ['[', file, '-rs', str(start_frame_shift), '-in', str(start_frame_shift + 1), ']']
+def _format_comp_arg(file, range_start):
+    return ['[', file, '-rs', str(range_start), ']']
 
 
-def _format_plate_arg(file, cut_in_frame):
-    return ['[', file, '-in', str(cut_in_frame + 1), ']']
+def _format_plate_arg(file, frame_offset):
+    return ['[', file, '-in', str(frame_offset), ']']
+
+
+def _get_frame_offset_and_range_start_from_version_data(record):
+    comp_start = record['VFXEditorialShots::CompStart']
+    frame_offset, range_start = 0, 0
+    if comp_start:
+        comp_start = int(comp_start)
+        if comp_start > 1000:
+            print(f'Comp start is {comp_start}')
+            frame_offset = comp_start - 1000
+            range_start = frame_offset
+
+    # vendor = record['Vendor'].lower().replace(' ', '').replace('.', '')
+    # if 'ih' in vendor:
+        # range_start += 1
+
+    return frame_offset, range_start
+
+
+def _find_version_data(filepath):
+    filename = os.path.basename(filepath)
+    version_name = filename.split('.')[0]
+    with CloudServerWrapper(url=FMP_URL,
+                            user=FMP_USERNAME,
+                            password=FMP_PASSWORD,
+                            database=FMP_VFX_DB,
+                            layout=FMP_VERSIONS_LAYOUT
+                            ) as fmp:
+        fmp.login()
+        query = {'Filename': version_name}
+        records = None
+        try:
+            records = fmp.find([query])
+        except:
+            if fmp.last_error == 401:
+                pass
+            else:
+                raise
+        return records
 
 
 def _find_latest_dnx(files):
